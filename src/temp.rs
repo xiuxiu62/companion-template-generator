c034976a72b980df1e08baf40e933a9257fa5486
xiuxiu62 <jacremer@live.com>
2022-09-06 21:48:55 -0700

    initial

---
+++ src/temp.rs
@@ -0,0 +1,202 @@
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct Config {
+        #[serde(rename = "1")]
+_1: PageConfig,
+    #[serde(rename = "2")]
+_2: PageConfig,
+    #[serde(rename = "3")]
+_3: PageConfig,
+    #[serde(rename = "4")]
+_4: PageConfig,
+    #[serde(rename = "5")]
+_5: PageConfig,
+    #[serde(rename = "6")]
+_6: PageConfig,
+    #[serde(rename = "7")]
+_7: PageConfig,
+    #[serde(rename = "8")]
+_8: PageConfig,
+    #[serde(rename = "9")]
+_9: PageConfig,
+    #[serde(rename = "10")]
+_10: PageConfig,
+    #[serde(rename = "11")]
+_11: PageConfig,
+    #[serde(rename = "12")]
+_12: PageConfig,
+    #[serde(rename = "13")]
+_13: PageConfig,
+    #[serde(rename = "14")]
+_14: PageConfig,
+    #[serde(rename = "15")]
+_15: PageConfig,
+    #[serde(rename = "16")]
+_16: PageConfig,
+    #[serde(rename = "17")]
+_17: PageConfig,
+    #[serde(rename = "18")]
+_18: PageConfig,
+    #[serde(rename = "19")]
+_19: PageConfig,
+    #[serde(rename = "20")]
+_20: PageConfig,
+    #[serde(rename = "21")]
+_21: PageConfig,
+    #[serde(rename = "22")]
+_22: PageConfig,
+    #[serde(rename = "23")]
+_23: PageConfig,
+    #[serde(rename = "24")]
+_24: PageConfig,
+    #[serde(rename = "25")]
+_25: PageConfig,
+    #[serde(rename = "26")]
+_26: PageConfig,
+    #[serde(rename = "27")]
+_27: PageConfig,
+    #[serde(rename = "28")]
+_28: PageConfig,
+    #[serde(rename = "29")]
+_29: PageConfig,
+    #[serde(rename = "30")]
+_30: PageConfig,
+    #[serde(rename = "31")]
+_31: PageConfig,
+    #[serde(rename = "32")]
+_32: PageConfig,
+    #[serde(rename = "33")]
+_33: PageConfig,
+    #[serde(rename = "34")]
+_34: PageConfig,
+    #[serde(rename = "35")]
+_35: PageConfig,
+    #[serde(rename = "36")]
+_36: PageConfig,
+    #[serde(rename = "37")]
+_37: PageConfig,
+    #[serde(rename = "38")]
+_38: PageConfig,
+    #[serde(rename = "39")]
+_39: PageConfig,
+    #[serde(rename = "40")]
+_40: PageConfig,
+    #[serde(rename = "41")]
+_41: PageConfig,
+    #[serde(rename = "42")]
+_42: PageConfig,
+    #[serde(rename = "43")]
+_43: PageConfig,
+    #[serde(rename = "44")]
+_44: PageConfig,
+    #[serde(rename = "45")]
+_45: PageConfig,
+    #[serde(rename = "46")]
+_46: PageConfig,
+    #[serde(rename = "47")]
+_47: PageConfig,
+    #[serde(rename = "48")]
+_48: PageConfig,
+    #[serde(rename = "49")]
+_49: PageConfig,
+    #[serde(rename = "50")]
+_50: PageConfig,
+    #[serde(rename = "51")]
+_51: PageConfig,
+    #[serde(rename = "52")]
+_52: PageConfig,
+    #[serde(rename = "53")]
+_53: PageConfig,
+    #[serde(rename = "54")]
+_54: PageConfig,
+    #[serde(rename = "55")]
+_55: PageConfig,
+    #[serde(rename = "56")]
+_56: PageConfig,
+    #[serde(rename = "57")]
+_57: PageConfig,
+    #[serde(rename = "58")]
+_58: PageConfig,
+    #[serde(rename = "59")]
+_59: PageConfig,
+    #[serde(rename = "60")]
+_60: PageConfig,
+    #[serde(rename = "61")]
+_61: PageConfig,
+    #[serde(rename = "62")]
+_62: PageConfig,
+    #[serde(rename = "63")]
+_63: PageConfig,
+    #[serde(rename = "64")]
+_64: PageConfig,
+    #[serde(rename = "65")]
+_65: PageConfig,
+    #[serde(rename = "66")]
+_66: PageConfig,
+    #[serde(rename = "67")]
+_67: PageConfig,
+    #[serde(rename = "68")]
+_68: PageConfig,
+    #[serde(rename = "69")]
+_69: PageConfig,
+    #[serde(rename = "70")]
+_70: PageConfig,
+    #[serde(rename = "71")]
+_71: PageConfig,
+    #[serde(rename = "72")]
+_72: PageConfig,
+    #[serde(rename = "73")]
+_73: PageConfig,
+    #[serde(rename = "74")]
+_74: PageConfig,
+    #[serde(rename = "75")]
+_75: PageConfig,
+    #[serde(rename = "76")]
+_76: PageConfig,
+    #[serde(rename = "77")]
+_77: PageConfig,
+    #[serde(rename = "78")]
+_78: PageConfig,
+    #[serde(rename = "79")]
+_79: PageConfig,
+    #[serde(rename = "80")]
+_80: PageConfig,
+    #[serde(rename = "81")]
+_81: PageConfig,
+    #[serde(rename = "82")]
+_82: PageConfig,
+    #[serde(rename = "83")]
+_83: PageConfig,
+    #[serde(rename = "84")]
+_84: PageConfig,
+    #[serde(rename = "85")]
+_85: PageConfig,
+    #[serde(rename = "86")]
+_86: PageConfig,
+    #[serde(rename = "87")]
+_87: PageConfig,
+    #[serde(rename = "88")]
+_88: PageConfig,
+    #[serde(rename = "89")]
+_89: PageConfig,
+    #[serde(rename = "90")]
+_90: PageConfig,
+    #[serde(rename = "91")]
+_91: PageConfig,
+    #[serde(rename = "92")]
+_92: PageConfig,
+    #[serde(rename = "93")]
+_93: PageConfig,
+    #[serde(rename = "94")]
+_94: PageConfig,
+    #[serde(rename = "95")]
+_95: PageConfig,
+    #[serde(rename = "96")]
+_96: PageConfig,
+    #[serde(rename = "97")]
+_97: PageConfig,
+    #[serde(rename = "98")]
+_98: PageConfig,
+    #[serde(rename = "99")]
+_99: PageConfig,
+
+}
